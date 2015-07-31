f9ab2a6fe7c9f014ed841ddafd9e078fe064cef1
Alena <alena.simonova83@gmail.com>
2015-07-31 18:51:06 +0300

    3107

---
+++ app/scripts/carousel.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(".calculator-types__controls").waterwheelCarousel({
+      // include options like this:
+      // (use quotes only for string values, and no trailing comma after last option)
+      // option: value,
+      // option: value
+    });
+  });
--- app/scripts/common.js
+++ app/scripts/common.js
@@ -1,27 +1,27 @@
-//скроллинг offers
-;$(document).ready(function() {
-  var sliders = $('.offers-item-slider');
-  
-  function sizing_track(num, size) {
-    if (num[(num.length - 1)] === '%') {
-       num = num.slice(0, -1) / 100;
-    } else {
-      num = num.slice(0, -2) / size.slice(0, -2);
-    }
-    return num;
-  }
-  
-  for(var i = 0; i < sliders.length; i++) {
-    var slider = $(sliders[i]);
-    
-    slider.tinyscrollbar({
-        axis: "x"
-      ,	trackSizePer: sizing_track(slider.find('.scrollbar').css('width'), slider.css('width'))
-      ,	thumbSize: 80
-      , mainContainer: $('.offers-slider__box')
-    });
-  };
-});
+////скроллинг offers
+//;$(document).ready(function() {
+//  var sliders = $('.offers-item-slider');
+//  
+//  function sizing_track(num, size) {
+//    if (num[(num.length - 1)] === '%') {
+//       num = num.slice(0, -1) / 100;
+//    } else {
+//      num = num.slice(0, -2) / size.slice(0, -2);
+//    }
+//    return num;
+//  }
+//  
+//  for(var i = 0; i < sliders.length; i++) {
+//    var slider = $(sliders[i]);
+//    
+//    slider.tinyscrollbar({
+//        axis: "x"
+//      ,	trackSizePer: sizing_track(slider.find('.scrollbar').css('width'), slider.css('width'))
+//      ,	thumbSize: 80
+//      , mainContainer: $('.offers-slider__box')
+//    });
+//  };
+//});
 
 //всплывающие описания offers
 ;$(document).ready(function() {
@@ -86,41 +86,7 @@ slide.hover(function () {
 });
 });
 
-//скроллинг formates
-;$(document).ready(function() {
-  var controls = $('.formates-slider__controls label');
-var sliders = $('.formates-item-slider');
-
-function scroll(slide) {
-  slide.tinyscrollbar({
-      axis: "y"
-    ,	thumbSize: 120
-  });
-  
-  var scrollbar = slide.data("plugin_tinyscrollbar")
-scrollbar.update();
-};
-
-function search(items) {
-  for(var i = 0; i < items.length; i++) {
-    var item = $(items[i]);
-    if(item.is(':visible')) {
-      return item;
-    };
-  };
-};
-
-scroll(search(sliders));
-
-controls.click(function() {
-  var mask = $(this).attr('for').replace(/[\w|-]*--([\w|-]*)/gi, '$1');
-
-  setTimeout(function() {
-    scroll($('.formates-item-slider--' + mask));
-  }, 1);
-});
-});
-
+//formates
 ;$(document).ready(function() {
 	var slides = $('.formates-item__item');
 	var close = $('.formates-item__item--pops-close');
